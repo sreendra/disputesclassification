1e1f3cbfb5d1fa6ab827153f1d3eb2522ab91e99
rachikkala <rachikkala@paypal.com>
2018-04-24 22:25:40 +0530

    Added support for loading training.

--- src/main/java/com/abc/disputes/classification/data/models/Corpus.java
+++ src/main/java/com/abc/disputes/classification/data/models/Corpus.java
@@ -1,17 +1,27 @@
 package com.abc.disputes.classification.data.models;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import io.vavr.control.Try;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import javax.management.ImmutableDescriptor;
+
+import static java.util.stream.Collectors.toList;
 
 public class Corpus {
 
     //https://en.wikipedia.org/wiki/Tf-idf and some places i saw the factor set to 0.4. We'll
     //try both the options.
     private static final double TFIDF_SMOOTHING_FACTOR = 0.5;
+    private Logger logger = LoggerFactory.getLogger(Corpus.class);
 
     private Map<String,Integer> keyColIdMap;
     private Map<Integer,List<DocumentRow>> disputeClassDocsMap;
@@ -93,6 +103,38 @@ public class Corpus {
                 disputeClassDocsMap.get(disputeClass).stream().map(documentRow -> documentRow.getTermFreqMap().containsKey(term) ? 1 :0).reduce(0,Integer::sum)
         ).reduce(0,Integer::sum);
 
+    }
+
+    public List<DisputeDocument> readTrainingDocs() {
+
+        List<String> validSheets = Arrays.asList("bdoToInr","otherToInr","bdoToSnad","otherToSnad","bdoToUnauth");
+        Map<String,Integer> sheetNameToClassMap = new HashMap<String,Integer>() {/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+		{
+            put("bdoToInr",0);
+            put("otherToInr",0);
+            put("bdoToSnad",1);
+            put("otherToSnad",1);
+            put("bdoToUnauth",2);
+        }
+		};
+
+        return Stream.of("may","jul","aug").
+                map(month -> "train_data/"+month+"_disputes.xlsx").
+                flatMap(fileName ->
+                        Try.of( () ->StreamSupport.stream(WorkbookFactory.create(new File(this.getClass().getClassLoader().getResource(fileName).getFile())).spliterator(),false)).
+                                onFailure(throwable -> logger.error("Exception while loading data",throwable)).get().
+                                filter(sheet -> validSheets.contains(sheet.getSheetName())).
+                                flatMap(sheet -> StreamSupport.stream(sheet.spliterator(),false)).
+                                skip(1).
+                                filter(row -> row.getCell(1) != null).
+                                //limit(10).
+                                map(row -> new DisputeDocument(row.getCell(1).toString(),sheetNameToClassMap.get(row.getSheet().getSheetName()))).
+                                collect(toList()).stream()
+                ).collect(toList());
     }
 
 }
